a5a2abfb5e1ee898efb10e7641eea2d472ea8476
Alejandro Salas <asalas.wd@outlook.com>
2022-01-19 16:31:50 -0500

    add basic functions

--- mars_robot.js
+++ mars_robot.js
@@ -1,1 +1,13 @@
 // This is my Mars robot
+
+function turnLeft(rover) {
+  console.log('turnLeft was called!');
+}
+ 
+function turnRight(rover) {
+  console.log('turnRight was called!');
+}
+ 
+function moveForward(rover) {
+  console.log('moveForward was called');
+}
