74c541137180896840f3b5174bb5111a9edbee9c
Alejandro Salas <asalas.wd@outlook.com>
2022-02-03 22:23:29 -0500

    create moveForward function

--- mars_robot.js
+++ mars_robot.js
@@ -49,13 +49,37 @@ console.log('The rover is now facing ' + rover.direction);
 }
  
 function moveForward(rover) {
+  console.log('The rover is currently facing ' + rover.direction);
+  console.log('And located at x: ' + rover.x + ', y: ' + rover.y);
+
+  switch(rover.direction){
+    case "N":
+    rover.y--;
+    break;
+
+    case "E":
+    rover.x++;
+    break;
+
+    case "S":
+    rover.y++;
+    break;
+
+    case "W":
+    rover.x--;
+    break;
+  }
   console.log('moveForward was called');
+  console.log('The rover is now facing ' + rover.direction);
+  console.log('And located at x: ' + rover.x + ', y: ' + rover.y);
 }
 
 // Create robot with property "direction"
 
 let robot = {
-  direction: 'N'
+  direction: 'N',
+  x: 0,
+  y: 0
 }
 
 turnLeft(robot);
